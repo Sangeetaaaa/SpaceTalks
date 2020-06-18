c4c7106ad886db8ad7fe5019f2d272e683fac120
Sangeeta <saangeeeeta@gmail.com>
2020-06-18 19:45:49 +0530

    change port

--- app.js
+++ app.js
@@ -128,11 +128,14 @@ app.post("/add", (req, res) => {
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 
 
 
-
-app.listen(3000 || process.env.port, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
